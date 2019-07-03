21c0176a1184b6d9ffce6c9e418dbd66de4534af
Rob Trimpe <rtrimpe@gmail.com>
2019-07-03 11:16:41 -0400

    abstract fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,7 +51,7 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
                 return;
             }
-            gradeBook = new BaseGradeBook(name);
+            //gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
